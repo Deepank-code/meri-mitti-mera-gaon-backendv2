7cc2418e864d8ea11223bafd41c1e2cef096b690
Deepank-code <82895981+Deepank-code@users.noreply.github.com>
2024-12-24 19:44:01 +0530

    resolved typescript issue

--- src/utils/features.ts
+++ src/utils/features.ts
@@ -95,30 +95,27 @@ interface MyDocument extends Document {
   discount?: number;
   total?: number;
 }
-export const getChartData = ({
-  length,
-  docArr,
-  today,
-  property,
-}: {
-  length: number;
-  docArr: MyDocument[];
+export const getChartData = ({ length, docArr, today, property }: any) =>
+  // : {
+  //   length: number;
+  //   docArr: MyDocument[];
 
-  today: Date;
-  property?: "discount" | "total";
-}) => {
-  const data: number[] = new Array(length).fill(0);
-  docArr.forEach((i) => {
-    const creationDate: Date = i.createdAt;
-    const monthDiff = (today.getMonth() - creationDate.getMonth() + 12) % 12;
+  //   today: Date;
+  //   property?: "discount" | "total";
+  // }
+  {
+    const data: number[] = new Array(length).fill(0);
+    docArr.forEach((i: any) => {
+      const creationDate: Date = i.createdAt;
+      const monthDiff = (today.getMonth() - creationDate.getMonth() + 12) % 12;
 
-    if (monthDiff < length) {
-      if (property) {
-        data[length - monthDiff - 1] += i[property]!;
-      } else {
-        data[length - monthDiff - 1] += 1;
+      if (monthDiff < length) {
+        if (property) {
+          data[length - monthDiff - 1] += i[property]!;
+        } else {
+          data[length - monthDiff - 1] += 1;
+        }
       }
-    }
-  });
-  return data;
-};
+    });
+    return data;
+  };
